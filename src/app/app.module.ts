e2afb32c11f080106b20fb03d60420164f47c495
Gerrit Schöne <gs@embeteco.de>
2020-04-02 11:33:41 +0200

    * Fixed Contact Form for Netlify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +27,15 @@ import { ContactComponent } from './components/contact/contact.component';
     FooterComponent,
     ContactComponent
   ],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, FontAwesomeModule, CarouselModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    CarouselModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { FormControl, FormGroup } from '@angular/forms';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -8,8 +10,45 @@ import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 })
 export class ContactComponent implements OnInit {
   faPaperPlane = faPaperPlane;
+  contactForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    message: new FormControl('')
+  });
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    const body = new HttpParams()
+      .set('form-name', 'contact')
+      .append('name', this.contactForm.value.name)
+      .append('email', this.contactForm.value.email)
+      .append('art', this.contactForm.value.art)
+      .append('message', this.contactForm.value.message);
+    this.http
+      .post('/', body.toString(), { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+      .subscribe(
+        res => {},
+        err => {
+          if (err instanceof ErrorEvent) {
+            //client side error
+            alert('Something went wrong when sending your message.');
+            console.log(err.error.message);
+          } else {
+            //backend error. If status is 200, then the message successfully sent
+            if (err.status === 200) {
+              alert('Your message has been sent!');
+            } else {
+              alert('Something went wrong when sending your message.');
+              console.log('Error status:');
+              console.log(err.status);
+              console.log('Error body:');
+              console.log(err.error);
+            }
+          }
+        }
+      );
+  }
 
   ngOnInit(): void {}
 }
